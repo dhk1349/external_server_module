37d6f4366c17c6ff840fb00948b0fb5ffb6140b9
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-08-13 17:46:15 +0900

    .

---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 22:33:18 2020
+
+@author: dhk13
+"""
+from flask import Flask, jsonify, request
+import tensorflow as tf
+import numpy as np
+
+# from config import basedir
+
+# import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    return "hello"
+
+
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    if request.method == 'POST':
+        print("0")
+        result=request.json
+        print(result)
+        angles=[]
+        angles.append(np.array([result['0'], result['1'], result['2']]))
+        angles=np.array(angles)
+        print("1")
+        print(angles)
+        model = tf.keras.models.load_model("mymodel2.h5")
+        print("model loaded")
+        pred = model.predict(angles)
+        print(pred)
+        result={}
+        result['0']=str(pred[0][0])
+        result['1']=str(pred[0][1])
+    return result
+
+
+if __name__ == '__main__':
+        app.run(debug=True, host="0.0.0.0", port=5000)
+
